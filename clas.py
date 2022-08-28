42d0f18dbf950a9ffe2300fce2b1b35b981fbdfd
kiran <kiran@gmail.com>
2022-08-28 23:22:54 +0530

    adddedd

--- clas.py
+++ clas.py
@@ -1,4 +1,23 @@
-a=int(input("Enter A:")) 
-a=int(input("Enter B:"))
-area=(22/7)*r**2
-print(area)
+#import xlsxwriter module     
+# 
+import xlsxwriter
+      
+book = xlsxwriter.Book('Example2.xlsx')     
+sheet = book.add_sheet()     
+       
+# Rows and columns are zero indexed.     
+row = 0    
+column = 0    
+      
+content = ["Parker", "Smith", "John"]     
+      
+# iterating through the content list     
+for item in content :     
+      
+    # write operation perform     
+    sheet.write(row, column, item)     
+      
+    # incrementing the value of row by one with each iterations.     
+    row += 1    
+          
+book.close()     
--- list1.py
+++ list1.py
@@ -1,2 +1,11 @@
-list2=[4,7,4]
-print(list2)
+time = 4
+if (time>=8 or time >= 10):
+    print("Its science class")
+elif(time>=11 or time<= 1):
+    print("Its maths class")
+elif(time >= 2 or time <4):
+    print("Its EVS class")
+elif(time >= 4 or time <= 5):
+    print("Its time for PE period enjoy")
+else:
+    print("Time to go home, Bye")
